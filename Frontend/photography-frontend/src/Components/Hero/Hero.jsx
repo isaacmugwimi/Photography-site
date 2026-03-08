53dfd5d2633612e64f511a41b6fc99dfe42d5801
Isaac <mugwimiisaac230@gmail.com>
2026-03-08 22:19:45 +0300

    added a navbar and landing page

--- Frontend/photography-frontend/src/Components/Hero/Hero.jsx
+++ Frontend/photography-frontend/src/Components/Hero/Hero.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import React from "react";
+import "./Hero.css";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="hero">
+      <div className="hero-content">
+        <div className="hero-tag">
+          <span className="hero-dot"></span>
+          <span>premium photography and videography services</span>
+        </div>
+        <h1>
+          Capture Your <br /> <span>Moments</span>
+        </h1>
+        <p class="hero-description">
+          Professional photography, videography, and creative services that
+          bring your vision to life. From advertisements to events, we craft
+          visual stories that resonate.
+        </p>
 
-export default Hero
+        <div className="hero-btn-section">
+          <button className="hero-btns">View Portfolio</button>
+          <button className="hero-btns">Explore Courses</button>
+        </div>
+
+        <div class="hero-stats">
+              <div class="hero-stat">
+                <div class="hero-stat-value">500+</div>
+                <div class="hero-stat-label">Projects Done</div>
+              </div>
+              <div class="hero-stat">
+                <div class="hero-stat-value">200+</div>
+                <div class="hero-stat-label">Happy Clients</div>
+              </div>
+              <div class="hero-stat">
+                <div class="hero-stat-value">10+</div>
+                <div class="hero-stat-label">Years Experience</div>
+              </div>
+            </div>
+      </div>
+      <div className="hero-content-left-image"></div>
+    </section>
+  );
+};
+
+export default Hero;
--- Frontend/photography-frontend/src/Components/NavBar/NavBar.jsx
+++ Frontend/photography-frontend/src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css"
 
 const NavBar = () => {
   return (
-    <div>NavBar hello</div>
-  )
-}
+    <header>
+      <nav className="navbar">
+        <div className="logo">Lens.</div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <div className="book-now-btn">
+          <button>
+            Book Now
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default NavBar
+export default NavBar;
